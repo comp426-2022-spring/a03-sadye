97c38293feb6de5f54060ba64f09311ddbf508d7
sadye <sadye@live.unc.edu>
2022-02-23 14:39:32 -0500

    added dependencies -- added endpoints

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 const express = require('express')
+const args = require('minimist')(process.argv.slice(2)) 
 const app = express()
 
-var port = 5000 // add command line argument
+args['port']
+var port = args[port] | 5000 // add command line argument
 
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 })
 
 app.use(function(req, res) {
-    res.status(404).send("ENDPOINT DOES NOT EXIST")
+    res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
 
 })
+
+app.get('/app/', (req, res) => {
+    //Respond w status 200
+    res.statusCode = 200;
+    //Respond w status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, {
+        'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' + res.statusMessage)
+});
+
+app.get('/app/flips/:number', (req, res) => {
+	//Some
+	//expressions
+	//go
+	//here
+});
+
+app.get('/app/flip/call/:call', (req, res) => {
+	//Some
+	//expressions
+	//go
+	//here
+});
+
+function coinFlip() {
+    return(Math.floor(Math.random()*2)==0)? 'heads':'tails';
+  }
+
+app.get('/app/flip/', (req, res) => {
+    var flip = coinFlip()
+	res.statusCode(200).json({ 'flip ' : flip})
+});
